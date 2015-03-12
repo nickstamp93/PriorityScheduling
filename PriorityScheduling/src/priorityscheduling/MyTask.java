d4d42856d8dd57545594483abe2a752e1e4a4d12
Stampoulis Nikolaos <nickstamp93@gmail.com>
2015-03-12 18:52:33 +0200

    Basic Variables + Constructors
    Added some basic variables int the MyTask and Semaphore classes and
    implemented a constructor for each of them.

--- PriorityScheduling/src/priorityscheduling/MyTask.java
+++ PriorityScheduling/src/priorityscheduling/MyTask.java
@@ -1,15 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package priorityscheduling;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author Nikos
+ * @author Nikos Stampoulis
  */
-public class MyTask {
+public class MyTask extends Thread{
+    private int priority;   //priority
+    private int pid;    //proccess id
+    private int arrivalTime;    //arrival time of proccess after start of the programm (seconds)
+    private Semaphore s; //the common semaphore object among the tasks
+    private int executionTime;  // time using the resource (seconds)
     
+    
+    MyTask(int pid, int priority, int arrivalTime,int executionTime, Semaphore s){
+        this.priority = priority;
+        this.pid = pid;
+        this.arrivalTime = arrivalTime;
+        this.s = s;
+        this.executionTime = executionTime;
+    }
+    
+    public void setSemaphore(Semaphore sem){
+    }
+    
+    public int getTaskPriority(){
+        return priority;
+    }
+    
+    public int getID(){
+        return pid;
+    }
+    public int getArrivalTime(){
+        return arrivalTime;
+    }
 }
--- PriorityScheduling/src/priorityscheduling/Semaphore.java
+++ PriorityScheduling/src/priorityscheduling/Semaphore.java
@@ -1,15 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package priorityscheduling;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Nikos Stampoulis
  */
 public class Semaphore {
+    
+    private static boolean isBusy;    //true , if resource is currently in use
+    private static boolean[] isWaiting;    //isWaiting[i] = true if task i is currently waiting to use the resource
+    private static int[] priorities;    //priorities[i] is the priority of task i
+    
+    //array with tasks that are in sleep mode
+    private static MyTask[] sleepTasks;
+    
+    MyTask handler;
+
+    Semaphore(int numThreads){
+        isBusy = false;
+        isWaiting = new boolean[numThreads];
+        priorities = new int[numThreads];
+        
+        for(boolean b : isWaiting){
+            b=false;
+        }
+        sleepTasks = new MyTask[numThreads];
+    }
+    
+    
     
 }
